271bece52cf1de172f751f96d7ebaa7ac0f06ef9
Tom Scharstein <trscharstein@gmail.com>
2019-07-16 22:16:13 -0700

    Fixing lodash, replacing stylizer with starlette

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <div id="app">
     <!-- This is the root of your panel -->
     <!-- Content should go inside #app -->
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue CLI panel"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue CLI panel" />
   </div>
 </template>
 
 <script>
 // You can access this App.vue file from any other component via `this.$root.$children[0]`
-// See `./components/HelloWorld.vue` for example of CSInteface and app
+// See `./components/HelloWorld.vue` for example of CSInterface and this.app
 
 // Create your own components and import them here
 import HelloWorld from "./components/HelloWorld.vue";
+
+// Dynamic CSS variables that automatically handle all app themes and changes:
+// https://github.com/Inventsable/starlette
+import starlette from "starlette";
 
 export default {
   name: "app",
@@ -24,6 +28,7 @@ export default {
   }),
   mounted() {
     this.csInterface = new CSInterface();
+    starlette.init();
   },
   methods: {
     dispatchEvent(name, data) {
@@ -40,11 +45,49 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/* Various helper styles to match application theme */
+@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
+:root {
+  background-color: var(--color-bg);
+  color: var(--color-default);
+  font-family: "Open Sans", sans-serif;
+  font-size: 10px;
+}
+
+#app::-webkit-scrollbar {
+  display: block;
+}
+body::-webkit-scrollbar {
+  width: 0px;
+}
+
+::-webkit-scrollbar {
+  background-color: var(--color-scrollbar);
+  width: var(--width-scrollbar-track);
+}
+::-webkit-scrollbar-thumb {
+  width: var(--width-scrollbar-track);
+  background: var(--color-scrollbar-thumb);
+  border-radius: var(--radius-scrollbar-thumb);
+}
+::-webkit-scrollbar-thumb:hover {
+  background: var(--color-scrollbar-thumb-hover);
+}
+::-webkit-scrollbar-resizer {
+  display: none;
+  width: 0px;
+  background-color: transparent;
+}
+::-webkit-scrollbar-button {
+  height: 0px;
+}
+::-webkit-scrollbar-corner {
+  display: none;
 }
 </style>
